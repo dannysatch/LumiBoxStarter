def32faed62ff55e847b5a479b2e96751f51727a
LumiQAJamie <144437179+LumiQAJamie@users.noreply.github.com>
2025-07-23 15:03:59 +0100

    Added if statament to check if stages have been completed.ps1

--- boxstarter.ps1
+++ boxstarter.ps1
@@ -1,9 +1,30 @@
 
-# Install Google Chrome
-choco install firefox -y
+# # Install Google Chrome
+# choco install firefox -y
 
-# Reboot the system
-Invoke-Reboot
+# # Reboot the system
+# Invoke-Reboot
 
-# After reboot, install VS Code
-choco install vscode -y
+# # After reboot, install VS Code
+# choco install vscode -y
+
+
+$firefoxInstalled = Test-Path "C:\ProgramData\Boxstarter\SetupFlags\Firefox.txt"
+$vscodeInstalled  = Test-Path "C:\ProgramData\Boxstarter\SetupFlags\VSCode.txt"
+
+if (-not $firefoxInstalled) {
+    Write-Host "`n Installing Firefox..."
+    choco install firefox -y
+    New-Item -ItemType File -Path "C:\ProgramData\Boxstarter\SetupFlags\Firefox.txt" | Out-Null
+
+    Write-Host "`n Rebooting to continue setup..."
+    Invoke-Reboot
+  }
+
+if (-not $vscodeInstalled) {
+      Write-Host "`n Installing VS Code..."
+      choco install vscode -y
+      New-Item -ItemType File -Path "C:\ProgramData\Boxstarter\SetupFlags\VSCode.txt" | Out-Null
+  }
+
+    Write-Host "`n Setup complete!"
