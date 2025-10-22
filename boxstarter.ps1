7a836724f6b590b5be12bca26743ec83183bbc31
LumiQAJamie <144437179+LumiQAJamie@users.noreply.github.com>
2025-10-22 12:12:38 +0100

    Added Script 3

--- boxstarter.ps1
+++ boxstarter.ps1
@@ -22,10 +22,14 @@ Remove-Item $zipFilePath
 
 Write-Host "Files extracted to: $workDir"
 
+# Script 0 (Set ExecutionPolicy)
 Start-Process "cmd.exe" -ArgumentList "/c `"$workDir\0_AllowPowershell (right-click and run as administrator).cmd`"" -Verb RunAs -Wait
 
 # Script 2
 Start-Process "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -NoProfile -File `"$workDir\2_LumiComputerSetup-AfterInstallingOS.ps1`"" -Verb RunAs -Wait
+
+# Script 3
+Start-Process "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -NoProfile -File `"$workDir\3_LumiComputerSetup-AfterInstallingOS.ps1`"" -Verb RunAs -Wait
 
 $ErrorActionPreference = 'Stop'
 
